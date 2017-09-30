782ccfd595b1d1643cf9f6fb3973a852e7e45a42
Jase Pellerin <jasepellerin@gmail.com>
2017-09-30 16:16:22 -0600

    Got dark mode toggle working

--- source/scripts/components/App.vue
+++ source/scripts/components/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <v-app id="chat" toolbar footer dark>
-    <v-toolbar dark fixed>
+  <v-app id="chat" :dark="dark" toolbar footer>
+    <v-toolbar fixed>
       <v-toolbar-title>Mon Chat</v-toolbar-title>
+      <v-spacer />
+      <v-switch label="Dark mode" v-model="dark" />
     </v-toolbar>
     <main>
       <v-container fluid>
         <router-view></router-view>
       </v-container>
     </main>
-    <v-footer dark fixed>
-      <span class="white--text">Jase Pellerin 2017</span>
+    <v-footer fixed>
+      <span>Jase Pellerin 2017</span>
     </v-footer>
   </v-app>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return { dark: false }
+  }
+}
+</script>
