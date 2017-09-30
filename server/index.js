e2e4f601a24da280554ca755fc7ff02a2343d1f0
Jase Pellerin <jasepellerin@gmail.com>
2017-09-30 11:23:32 -0600

    Got express static working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 
+app.use('/dist', express.static(path.join(__dirname, '../dist')))
+
 app.get('/', (req, res) => {
-  res.send('Hi')
+  res.redirect('/dist/html')
 })
 
 app.listen(4000)
