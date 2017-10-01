fa5a929c00e0eaac94404648fa142c1d6f6fb6b4
Jase Pellerin <jasepellerin@gmail.com>
2017-10-01 07:42:24 -0600

    Further refactored rules

--- source/scripts/components/Login.vue
+++ source/scripts/components/Login.vue
@@ -1,22 +1,28 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-input @submit-message="onSubmit" label="Enter Username" :rules=[lengthRule] icon="play_arrow" />
+    <v-input @submit-message="onSubmit" label="Enter Username" :rules=[rules.length] icon="play_arrow" />
   </v-layout>
 </template>
 
 
 <script>
 export default {
+  data: function() {
+    return {
+      rules: {
+        length: (name) => {
+          if (name.length >= 20) {
+            return 'Username should be 20 characters or less'
+          } else return true
+        }
+      }
+    }
+  },
   methods: {
     onSubmit: function(msg) {
       // Update store username
       this.$store.commit('changeUsername', msg)
       this.$router.replace({ name: 'chat', params: { chatID: 10 } })
-    },
-    lengthRule: (name) => {
-      if (name.length >= 20) {
-        return 'Username should be 20 characters or less'
-      } else return true
     }
   }
 }
