7eb735ba081a94edd2268a27908d56d6bc6a5539
Jase Pellerin <jasepellerin@gmail.com>
2017-10-01 13:00:18 -0600

    Got messages syncing with server

--- source/scripts/components/ChatApp.vue
+++ source/scripts/components/ChatApp.vue
@@ -17,12 +17,26 @@ export default {
   methods: {
     onSubmit: function(msg) {
       // Add message to array
-      this.messages.push(
-        {
-          user: this.$store.state.username,
-          time: new Date().toLocaleTimeString(),
-          text: msg
-        })
+      const message = {
+        user: this.$store.state.username,
+        time: new Date().toLocaleTimeString(),
+        text: msg
+      }
+      // Add message to local messages
+      this.messages.push(message)
+      // Patch message array
+      fetch('/chat/' + this.chatID, {
+        method: 'PATCH',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ messages: this.messages })
+      }).then(res => res.json()).then(res => {
+        if (res && res.messages) {
+          this.messages = res.messages
+        }
+      })
     }
   }
 }
