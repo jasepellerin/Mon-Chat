0255f217da9ce2c92cf7a04567f749009326ba7f
Jase Pellerin <jasepellerin@gmail.com>
2017-10-02 15:30:15 -0600

    Added pm2 stuff for real

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'MonChat',
+    script: './server/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-34-213-238-156.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/aws-eb',
+      ref: 'origin/master',
+      repo: 'git@github.com:jasepellerin/Mon-Chat.git',
+      path: '/home/ubuntu/Mon-Chat',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
